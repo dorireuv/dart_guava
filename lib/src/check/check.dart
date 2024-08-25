d98e974c568ca194522b1412ef99a031187b691f
dorireuv <dori.public@gmail.com>
2024-08-25 10:46:47 +0300

    Add documentation for the check library.

--- lib/src/check/check.dart
+++ lib/src/check/check.dart
@@ -1,9 +1,11 @@
+/// Throws [ArgumentError] with [message] iff the given [isValid] is [False].
 void checkArgument(bool isValid, [String message = '']) {
   if (!isValid) {
     throw ArgumentError(message);
   }
 }
 
+/// Throws [StateError] with [message] iff the given [isValid] is [False].
 void checkState(bool isValid, [String message = '']) {
   if (!isValid) {
     throw StateError(message);
